0b0f195c7577fbac22e357f43152fc563a18ba39
Ulises <amao.apps@gmail.com>
2020-05-24 13:25:54 -0500

    Initial Commit

---
+++ DemoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoSwiftUI
+//
+//  Created by Rakso on 5/24/20.
+//  Copyright © 2020 Rakso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
